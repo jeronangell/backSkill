cbc78961cc5f3cf3e2fe7fb787cca83c4742432c
jeronangell <elder.angell@gmail.com>
2017-12-08 10:34:45 -0700

    Initial commit

---
+++ Defence.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+class Defence
+{
+public:
+	Defence();
+	~Defence();
+	
+	void sayHello() {
+		cout << "hello";
+	}
+	
+};
